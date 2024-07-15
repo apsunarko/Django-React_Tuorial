5356455dd43441fdc8914abea98cb87062f955a9
andreorimba <andre.sunarko@orimba.com>
2024-07-15 14:20:53 +0700

    Frontend Setup
    1. Installation using Vite
    
    2. install axios react-router-dom jwt-decode
    
    3. frontend organization
    
    4. setup axios in api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,23 @@
+// axios intercepter
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api 
+// from now, we use this api object instead of axios by default so send the different requests
